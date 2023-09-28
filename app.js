ee300acb23733c86bd71aa17d75cc5ddb19f366d
AVIJIT-PAL <134725277+AVIJIT-PAL@users.noreply.github.com>
2023-09-29 01:58:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Import Three.js
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+// import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 
 // Create a scene
 const scene = new THREE.Scene();
