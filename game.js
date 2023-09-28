830378836457d382cf481f9f621c1222742e07ba
AVIJIT-PAL <134725277+AVIJIT-PAL@users.noreply.github.com>
2023-09-28 22:32:38 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,13 @@
+// Define variables and constants
+const gameWidth = 800;
+const gameHeight = 600;
+
+// Set up the game canvas
+const canvas = document.createElement('canvas');
+canvas.width = gameWidth;
+canvas.height = gameHeight;
+document.body.appendChild(canvas);
+
+const ctx = canvas.getContext('2d');
+
+// Your game logic goes here
